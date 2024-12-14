6da50eb59111624560ac18f45f190afd44d57d7b
Rachel-Susholz <rochelsusholz@gmail.com>
2024-12-14 12:40:00 -0500

    initial commit

---
+++ WindowsFormsUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUWindowFormsFramework
+{
+    internal class WindowsFormsUtility
+    {
+    }
+
+    public static void FormatGridForSearchResults(DataGridView grid)
+        {
+            grid.AllowUserToAddRows = false;
+            grid.ReadOnly = true;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+}
