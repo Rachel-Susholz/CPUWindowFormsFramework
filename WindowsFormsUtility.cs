7045a1c97e951e3b04a4b8854e712e5f855d25e4
Rachel-Susholz <rochelsusholz@gmail.com>
2024-12-17 02:40:51 -0500

    Part 2 Session 15 HW

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CPUWindowFormsFramework
 {
-    internal class WindowsFormsUtility
+    public class WindowsFormsUtility
     {
-    }
 
-    public static void FormatGridForSearchResults(DataGridView grid)
+
+        public static void FormatGridForSearchResults(DataGridView grid)
         {
             grid.AllowUserToAddRows = false;
             grid.ReadOnly = true;
@@ -18,4 +20,30 @@ namespace CPUWindowFormsFramework
             grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
 
+        public static void SetListBinding(ComboBox lst, DataTable sourcedt, DataTable targetdt, string tablename)
+        {
+            lst.DataSource = sourcedt;
+            lst.ValueMember = tablename + "Id";
+            lst.DisplayMember = lst.Name.Substring(3);
+            lst.DataBindings.Add("SelectedValue", targetdt, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+        public static void SetControlBinding(Control ctrl, DataTable dt)
+        {
+            string propertyname = "";
+            string columnname = "";
+            string controlname = ctrl.Name;
+
+            if (controlname.StartsWith("txt") || controlname.StartsWith("lbl"))
+            {
+                propertyname = "Text";
+                columnname = controlname.Substring(3);
+            }
+
+            if (propertyname != "" && columnname != "")
+            {
+                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+            }
+
+        }
+    }
 }
